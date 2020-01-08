d4b34a5f4e7b4afa07e2a80fa96fbb980b1b77d1
devcmgm <49372756+devcmgm@users.noreply.github.com>
2020-01-08 16:21:14 +0000

    k

---
+++ exampleGenerateExport/getProjects.ps1
@@ -0,0 +1,32 @@
+
+if ($args.Count -gt 3) {
+  $SCCBaseURL = $args[0]
+  $token = $args[1]
+  $AppName = $args[2]
+  $AppVersion = $args[3]
+
+  $params = @{
+    Uri     = "$SCCBaseURL/api/v1/projectVersions?start=0&limit=200&fulltextsearch=false&includeInactive=false&myAssignedIssues=false&onlyIfHasIssues=false"
+    Headers = @{ 'Authorization' = "FortifyToken $token" }
+    Method  = 'GET'
+  }
+
+  
+  $found = -1
+  $results = Invoke-RestMethod @params
+  ForEach ($version in $results.data) {
+    if (  $version.project.name -eq $AppName -and $version.name -eq $AppVersion ) {
+      $found = $version.id
+    }
+    
+  }
+  if ( $found -eq "-1" ) {
+    "Info: Application: ({0}) - Version: ({1}) Not Found !" -f $AppName, $AppVersion
+  } Else {
+    $found
+  }
+}
+Else {
+
+  "Usage: getApplicationId.ps1 <SSC Base URL> <SSC REST API Token> <Application Name> <Application Version> "
+}
